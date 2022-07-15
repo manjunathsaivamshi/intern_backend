b0ebb08f411246f358c95806857615c158159bf2
Manju <krrishsai648@gmail.com>
2022-07-15 17:19:43 +0530

    commit

--- index.js
+++ index.js
@@ -4,22 +4,30 @@ import student from './model.js'
 import bodyParser from 'body-parser'
 import url from 'url'
 import path from 'path'
-
-const salt_factor=10
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors'
 
 const app=express()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use([
+    cors(),
     bodyParser.json({ limit: '30mb', extended: true}),
 ]);
 
-app.listen(5000,(err)=>{
-    if(!err)
-    console.log("listening @ 5000");
-else{
-    console.log("app can't listen");
-}
-})
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:3000"],
+        methods: ['GET','POST','PUT','DELETE']
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT,()=>{
+    console.log(`Server running at port ${PORT}`);
+});
 
 mongoose.connect('mongodb+srv://manjunathsaivamshi:manjunath@cluster0.axpjm.mongodb.net/mydb?retryWrites=true&w=majority',(err)=>{
     if(!err) console.log('Connected to db')
